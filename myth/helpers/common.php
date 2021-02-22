44657ddc6661b34fdd2393f103cf52cd19dd5f6f
Lonnie Ezell <lonnieje@gmail.com>
2021-02-22 00:23:03 -0600

    Add env command to provide default values when getting vaules from env.

--- myth/helpers/common.php
+++ myth/helpers/common.php
@@ -14,8 +14,21 @@ function dd(...$vars)
 }
 
 /**
+ * Get a value from the environment, or return the default value.
+ *
+ * @param string      $key
+ * @param string|null $default
+ *
+ * @return array|false|string|null
+ */
+function env(string $key, ?string $default) {
+    return getenv($key) ?? $default;
+}
+
+/**
  * View Service
  */
 function view() {
 	return \Myth\View::factory();
 }
+
